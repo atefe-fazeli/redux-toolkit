bd482167691c9aa074520560e3d86ddecc6635a7
atefe fazeli <atefefazeli1996@outlook.com>
2025-02-08 01:55:13 +0330

    cerate courses reducer

---
+++ redux-toolkit/src/Redux/courses.jsx
@@ -0,0 +1,20 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+export const getCoursesFromServer = createAsyncThunk(
+  "courses/getCoursesFromServer",
+  async (url) => {
+    axios.get(url).then((res) => res);
+  }
+);
+const slice = createSlice({
+  name: courses,
+  initialState: [],
+  reducers: {},
+  extraReducers: {
+    [getCoursesFromServer.pending]: () => {},
+    [getCoursesFromServer.fulfilled]: (state, action) => {},
+    [getCoursesFromServer.rejected]: (state, action) => {},
+  },
+});
+
+export default slice.reducer;
--- redux-toolkit/src/Redux/store.jsx
+++ redux-toolkit/src/Redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users";
+import coursesReducer from './courses'
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    courses:coursesReducer,
   },
   devTools: true,
 });
